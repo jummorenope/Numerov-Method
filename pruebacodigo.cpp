a9628f5fadd280cbc9a5599dfad8ecbf455727d0
ljcaviedesb <ljcaviedesb@unal.edu.co>
2019-10-21 15:52:19 -0700

    inicio codigo, nombrar variables

---
+++ pruebacodigo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+void imprimirVector( const vector< int > & );
+void recibirVector( vector< int > &);
+
+int main void()
+{
+  int N=1000;
+  vector< int > Psi(N);
+  g2 = 200;
+  vector< int > v(N);
+  doble ep = -0.9;
+  vector< int > k2(N);
+  doble l2 = (1.0/(N-1))*(1.0/(N-1));
+
+  
+    
+
+}
