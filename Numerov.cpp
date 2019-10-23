6cd4ad1a2d5c98d6111fab0ec14c03861001fd8f
jummorenope <jummorenope@unal.edu.co>
2019-10-23 12:02:40 -0500

    por fin coge todas las energias

--- Numerov.cpp
+++ Numerov.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <cstring>
 #include <cmath>
+#include <iomanip>
 
 void WaveFunc (std::vector <double>& Psi, std::vector <double> k2, int N, double l2);
 void Potential(std::vector <double>& k2,int N, double ep);
@@ -11,9 +12,10 @@ int g2 = 200;
 
 int main (void)
 {
+    std::setprecision(10);
     int N=1000;
     double l2 = (1.0/(N-1))*(1.0/(N-1));
-    double dep=0.01; //Variacion del ep
+    double dep=0.1; //Variacion del ep
     double tol=1e-9;
     std::vector <double> k2(N);
     std::vector <double> Psi(N);
@@ -22,60 +24,43 @@ int main (void)
     Psi[0]=0;
     Psi[1]=0.0001;
     double Psi1=0;
-    int ii=1;
-    int write=0;
+    //int ii=1;
+    int write=1;
     std::string name="data";
     std::string ext=".txt";
-    while(ii<=10)
+    
+    for( int ii=1;ii<=5;ii++)
     {
+
         Potential(k2, N, ep);
         WaveFunc(Psi,k2,N,l2);
-        //std::cout<<ii<<std::endl;
+        Psi1=Psi[N-1];
         
-        if(std::fabs(Psi[N-1])>tol&&write)
+        while(std::fabs(Psi1)>tol)
         {
-            //std::cout<<"hola"<<std::endl;
-            if(std::signbit(Psi1)!=std::signbit(Psi[N-1])){
-                ep-=dep;
-                dep=dep/10.0;
-            }
             ep+=dep;
-            Psi1=Psi[N-1];
-        }
-        
-        if(write!=1)
-        {
-            Psi1=Psi[N-1];
-            write=1;
-        }
-        
-        if(std::fabs(Psi[N-1])<tol)
-        {
-            std::string num=std::to_string(ii);
-            std::string file=name+num+ext;
-            char cstr[file.size() + 1];
-            strcpy(cstr, file.c_str());
-            std::ofstream data;
-            data.open(file);
-            for(double jj=0; jj<N; jj++)
+            Potential(k2, N, ep);
+            WaveFunc(Psi,k2,N,l2);
+            if(Psi1*Psi[N-1]<=0)
             {
-                data <<jj/N<<"\t"<<Psi[jj]<<"\n";
+                dep=-dep/2.0;
             }
-            data.close();
-            std::cout<<ep<<std::endl;
-            ii+=1;
-            dep=0.001;
-            eps[ii-1]=ep;
-            ep+=dep;
-            write=0;
-            
-            
+            Psi1=Psi[N-1];
         }
-        
+        std::ofstream data;
+        data.open(name+std::to_string(ii)+ext);
+        for(double jj=0; jj<N; jj++)
+        {
+            data <<jj/N<<"\t"<<Psi[jj]<<"\n";
+        }
+        data.close();
+        std::cout<<ep<<std::endl;
+        dep=0.1;
+        ep+=dep;
     }
     
     
-    
+
     return 0;
 }
 
