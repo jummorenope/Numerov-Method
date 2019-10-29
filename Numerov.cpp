f75269122675196035d353f2b63d01233c3a5114
jummorenope <jummorenope@unal.edu.co>
2019-10-29 04:55:01 -0500

    Quantum Harmonic Oscillator

--- Numerov.cpp
+++ Numerov.cpp
@@ -14,7 +14,8 @@ int N=1000;
 
 int main (void)
 {
-    std::setprecision(10);
+    std::cout<<std::setprecision(10);
+    double l = (1.0/(N-1));
     double l2 = (1.0/(N-1))*(1.0/(N-1));
     double dep=0.1; //Variacion del ep
     double tol=1e-9;
@@ -25,17 +26,18 @@ int main (void)
     double ep = -0.99;
     Psi[0]=0;
     Psi[1]=0.0001;
+    double ept=0;
+    double terror=0;
     double Psi1=0;
     std::string name="data";
     std::string ext=".txt";
-    
-    for( int ii=1;ii<=3;ii++)
+    for( int ii=1;ii<=10;ii++)
     {
 
+        double norm=0.0;
         WaveNumber(k2, V, ep);
         WaveFunc(Psi,k2,l2);
         Psi1=Psi[N-1];
-        
         while(std::fabs(Psi1)>tol)
         {
             ep+=dep;
@@ -51,10 +53,15 @@ int main (void)
         data.open(name+std::to_string(ii)+ext);
         for(double jj=0; jj<N; jj++)
         {
-            data <<jj/N<<"\t"<<Psi[jj]<<"\n";
+            
+            data <<jj/N<<"\t"<<Psi[jj]<<"\t"<<Psi[jj]*Psi[jj]<<"\n";
         }
         data.close();
+        
+        ept=ii*ii*M_PI*M_PI/(g2)-1.0;
+        terror=std::fabs(100.0*(ept-ep)/ept);
         std::cout<<ep<<std::endl;
+        //std::cout<<ep<<"\t"<<ept<<"\t"<<terror<<'%'<<std::endl;
         dep=0.1;
         ep+=dep;
     }
@@ -81,7 +88,8 @@ void Potential (std::vector <double>& V)
 {
     for(double ii=0;ii<N;ii++)
     {
-        V[ii]=-1.0;
-        //V[ii]=8*(ii/N-0.5)*(ii/N-0.5)-1.0;
+        //V[ii]=-1.0;
+        V[ii]=30*(ii/N-0.5)*(ii/N-0.5)-1.0;
+        //V[ii]=8*(ii/N-0.5)*(ii/N-0.5)*(ii/N-0.5)*(ii/N-0.5)-1.0;
     }
 }
